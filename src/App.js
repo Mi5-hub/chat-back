3292c75558ef423c9455d8b707ff9a5b60347571
Mi5-hub <mino.randimbiarilanto@gmail.com>
2022-01-25 16:50:36 +0300

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import io from 'socket.io-client'
 import React, { useState } from 'react'
 import Chat from './Chat';
-const socket = io.connect('http://localhost:3001');
+const socket = io.connect('https://chat-fb-client.herokuapp.com/');
 
 
 function App() {
